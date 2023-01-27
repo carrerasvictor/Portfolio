e942edca1e2d3274dbf3e13f5e34265114be0e98
Victor <titotortuga3@gmail.com>
2023-01-27 18:35:17 -0300

    termiinando menu e iniciando parte de sklls do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './shared/menu/menu.component';
+import  { SkillsComponent } from './shared/skills/skills.component';
 
 const routes: Routes = [
-  { path: 'menu', component: MenuComponent },
   { path: '', component: MenuComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'skills', component: SkillsComponent },
+
+
+
 
 ];
 
